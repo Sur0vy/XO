83918346c3a9a68f4f1abbab400e0ac93ed5a2dc
Sur0vy <Sur0vy@yandex.ru>
2015-09-25 10:08:55 +0300

    add MoveController logic

--- src/com/Sur0vy/controller/MoveController.java
+++ src/com/Sur0vy/controller/MoveController.java
@@ -2,18 +2,21 @@ package com.Sur0vy.controller;
 
 import com.Sur0vy.model.Field;
 import com.Sur0vy.model.Figure;
-import com.Sur0vy.model.exceptions.InvalidMoveException;
+import com.Sur0vy.model.exceptions.AlreadyOccupaiedException;
+import com.Sur0vy.model.exceptions.InvalidPointException;
 
 import java.awt.*;
 
 public class MoveController {
 
-//    public void applyFigure(final Field field,
-//                               final Point point,
-//                               final Figure figure) throws InvalidMoveException {
-//        if (field.getFigure(point) != null)
-//            throw new InvalidMoveException();
-//        field.setFigure(point, figure);
-//    }
+    public void applyFigure(final Field field,
+                            final Point point,
+                            final Figure figure) throws AlreadyOccupaiedException,
+                                                        InvalidPointException {
+        if (field.getFigure(point) != null) {
+            throw new AlreadyOccupaiedException();
+        }
+        field.setFigure(point, figure);
+    }
 
 }

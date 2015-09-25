11b7b5b2405ec848cf525d497975cfaa5aec309e
Sur0vy <Sur0vy@yandex.ru>
2015-09-25 10:50:40 +0300

    add MoveControllerTest

---
+++ test/com/Sur0vy/controller/MoveControllerTest.java
@@ -0,0 +1,29 @@
+package com.Sur0vy.controller;
+
+import com.Sur0vy.model.Field;
+import com.Sur0vy.model.Figure;
+import com.Sur0vy.model.exceptions.AlreadyOccupaiedException;
+import com.Sur0vy.model.exceptions.InvalidPointException;
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+public class MoveControllerTest {
+
+    @Test
+    public void testSetFigureWhenAlreadyOccupaied() throws Exception {
+
+        final MoveController moveController = new MoveController();
+        final Field field = new Field();
+        final Point inputPoint = new Point(0, 0);
+
+        moveController.applyFigure(field, inputPoint, Figure.O);
+        try {
+            moveController.applyFigure(field, inputPoint, Figure.O);
+            fail();
+        } catch (final AlreadyOccupaiedException e) {}
+    }
+
+}

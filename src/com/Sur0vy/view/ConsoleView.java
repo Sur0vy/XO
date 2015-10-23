4370b2aa635c09c508d576529cdf1f103f33ecf6
Sur0vy <Sur0vy@yandex.ru>
2015-10-23 11:53:20 +0300

    create consoleView

--- src/com/Sur0vy/view/ConsoleView.java
+++ src/com/Sur0vy/view/ConsoleView.java
@@ -1,42 +1,98 @@
 package com.Sur0vy.view;
 
+import com.Sur0vy.controller.CurrentMoveController;
+import com.Sur0vy.controller.MoveController;
+import com.Sur0vy.controller.WinnerController;
 import com.Sur0vy.model.*;
+import com.Sur0vy.model.exceptions.AlreadyOccupaiedException;
+import com.Sur0vy.model.exceptions.InvalidPointException;
+import com.Sur0vy.model.exceptions.NoWinnerException;
 
 import java.awt.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class ConsoleView /*implements ICoordinateGetter*/ {
 
-//    public Point getMoveCoordinate(final Field field) {
-//        return new Point(0, 0);
-//    }
-//
-//    void show(final Field field) {
-//        final StringBuilder fieldBuilder = new StringBuilder();
-//        for (int x = 0; x < field.getSize(); x++) {
-//            if (x != 0)
-//                generateSeparator(fieldBuilder);
-//            generateLine(field, x, fieldBuilder);
-//        }
-//        System.out.println(fieldBuilder.toString());
-//    }
-//
-//    void generateLine(final Field field,
-//                      final int x,
-//                      final StringBuilder sb) {
-//        for (int y = 0; y < field.getSize(); y++) {
-//            if (y != 0)
-//                sb.append("|");
-//            sb.append(" ");
-//            final Figure figure;
-//            figure = field.getFigure(new Point(y, x));
-//            sb.append(figure != null ? figure : " ");
-//            sb.append(" ");
-//        }
-//        sb.append("\n");
-//    }
-//
-//    void generateSeparator(final StringBuilder sb) {
-//        sb.append("~~~~~~~~~~~\n");
-//    }
+    private final CurrentMoveController currentMoveController = new CurrentMoveController();
+
+    private final WinnerController winnerController = new WinnerController();
+
+    private  final MoveController moveController = new MoveController();
+
+    public void show(final Game game) {
+        System.out.format("Game name: %s\n", game.getName());
+        final Field field = game.getField();
+        for (int y = 0; y < field.getSize(); y++) {
+            if (y != 0) {
+                printHorisontalSeparator();
+            }
+            printLine(field, y);
+        }
+    }
+
+    public boolean move(final Game game) {
+        final Field field = game.getField();
+        try {
+            final Figure winner = winnerController.getWinner(field);
+            System.out.format("Winner is: %s\n", winner);
+            return false;
+        } catch (NoWinnerException e) {
+            final Figure currentFigure = currentMoveController.currentMove(field);
+            if (currentFigure == null) {
+                System.out.println("No winner and no moves left!");
+                return false;
+            }
+            System.out.format("Please enter move point for %s:\n", currentFigure);
+            final Point point = askPoint();
+            try {
+                moveController.applyFigure(field, point, currentFigure);
+            } catch (InvalidPointException | AlreadyOccupaiedException e2) {
+                System.out.println("Point is invalid!");
+            }
+            return true;
+        }
+    }
+
+    private Point askPoint() {
+        return new Point(askCoordinate("X") - 1, askCoordinate("Y") - 1);
+    }
+
+    private int askCoordinate(final String coordinateName) {
+        System.out.format("Please input %s:", coordinateName);
+        final Scanner in = new Scanner(System.in);
+        try {
+            return in.nextInt();
+        } catch (final InputMismatchException e) {
+            System.out.println("Point is invalid!");
+            return askCoordinate(coordinateName);
+        }
+
+    }
+
+    private void printLine(final Field field,
+                           final int y) {
+        for (int x = 0; x < field.getSize(); x++) {
+            if (x != 0) {
+                printVerticalSeparator();
+            }
+            final Figure figure;
+            try {
+                figure = field.getFigure(new Point(x, y));
+            } catch (InvalidPointException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.print(figure != null ? figure : "   ");
+        }
+        System.out.println();
+    }
+
+    private void printHorisontalSeparator() {
+        System.out.println("-----------");
+    }
+
+    private void printVerticalSeparator() {
+        System.out.print("|");
+    }
 
 }
